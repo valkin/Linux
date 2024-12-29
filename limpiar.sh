623bf162c6f7f26b23592afb4cd03a59f3065bae
valkin <valkin100@gmail.com>
2024-12-28 22:04:35 -0600

    2812202405

--- limpiar.sh
+++ limpiar.sh
@@ -12,14 +12,14 @@ sudo swapoff -a
 echo "OK - Swap deshabilitado"
 echo "<---------------------------------------------------------->"
 sleep 2s; echo "Liberando pagecaches, dentries e inodes"
-sync;sysctl -w vm.drop_caches=3;sync
+sync; sudo sysctl -w vm.drop_caches=3; sync
 echo "OK - Server liberado"
 echo "<---------------------------------------------------------->"
 sleep 3s; echo "Habilitando la Swap"
 sudo swapon -a
 echo "OK - Swap habilitado"
 echo "<---------------------------------------------------------->"
-sleep 2s; free
+sleep 2s; free -h
 echo ".....TODO CORRECTO....."
 echo "<---------------------------------------------------------->"
 echo "<---------------------------------------------------------->"
